0b5f42f3683ed8dc9243ff660f1f6c6e79181c15
Yury Rogens <yury_linkin@hotmail.com>
2024-07-25 15:20:03 -0300

    associamos a acao ao evento de submit e vimoes alternativas para esse processo

---
+++ main.js
@@ -0,0 +1,32 @@
+import { generateReturnsArray } from './src/investmentGoals';
+
+const form = document.getElementById('investment-form');
+
+function renderProgression(evt) { 
+  evt.preventDefault();
+  const startingAmount = Number(form['startingAmount'].value);
+  //const startingAmount = Number(
+    //document.getElementById('starting-amount').value);
+  const additionalContribution = Number(
+    document.getElementById('additional-contribution').value);
+  const timeAmount = Number(document.getElementById('time-amount').value);
+  const timeAmountPeriod = document.getElementById('time-amount-period').value;
+  const returnRate = Number(
+    document.getElementById('return-rate').value);
+  const returnRatePeriod = document.getElementById('evaluation-period').value;
+  const taxRate = Number(
+    document.getElementById('tax-rate').value);
+
+  const returnsArray = generateReturnsArray(
+    startingAmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribution,
+    returnRate,
+    returnRatePeriod
+  ); 
+  
+  console.log(returnsArray);
+}  
+  
+form.addEventListener('submit', renderProgression);
--- src/investmentGoals.js
+++ src/investmentGoals.js
@@ -1,45 +1,58 @@
 function convertToMontlyReturnRate(yearlyReturnRate) {
-    yearlyReturnRate ** (1/12);
-}
-
-function generateReturnsArray(
-    startingAmount = 0, 
-    timeHorizon = 0, 
-    timePeriod = 'monthly', 
-    monthlyContribution = 0, 
-    returnRate = 0, 
-    returnTimeFrame = 'monthly'
-) {
+    return yearlyReturnRate ** (1 / 12);
+  }
+  
+  export function generateReturnsArray(
+    startingAmount = 0,
+    timeHorizon = 0,
+    timePeriod = "monthly",
+    monthlyContribution = 0,
+    returnRate = 0,
+    returnTimeFrame = "monthly"
+  ) {
     if (!timeHorizon || !startingAmount) {
-        throw new Error("Investimento e prazo devem ser preenchidos com valores positivos.")
+      throw new Error(
+        "Investimento inicial e prazo devem ser preenchidos com valores positivos."
+      );
     }
-
-    const finalReturnRate = returnTimeFrame === 'monthly' ? 1 + returnRate / 100 
-    : convertToMontlyReturnRate(1 + returnRate/100);
-
-    const finalTimeHorizon = timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
-
+  
+    const finalReturnRate =
+      returnTimeFrame === "monthly"
+        ? 1 + returnRate / 100
+        : convertToMontlyReturnRate(1 + returnRate / 100);
+  
+    const finalTimeHorizon =
+      timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+  
     const referenceInvestmentObject = {
-        investedAmount : startingAmount,
-        interestReturn: 0,
-        totalInterestReturns: 0, 
-        month: 0,
-        totalAmount: startingAmount,
+      investedAmount: startingAmount,
+      interestReturns: 0,
+      totalInterestReturns: 0,
+      month: 0,
+      totalAmount: startingAmount,
     };
-
-    const returnsArrays = [referenceInvestmentObject]
-    for (let timeReference = 1; timeReference <= finalTimeHorizon; timeReference++){
-        const totalAmount = (returnsArrays[timeReference - 1].totalAmount * finalReturnRate) + monthlyContribution;
-        const interestReturn = returnsArrays[timeReference - 1].totalAmount * finalReturnRate;
-        const investedAmount = startingAmount + monthlyContribution * timeReference;
-        const totalInterestReturns = totalAmount - investedAmount;
-        returnsArrays.push({
-            investedAmount,
-            interestReturn,
-            totalInterestReturns,
-            month: timeReference,
-            totalAmount,
-        }) 
+  
+    const returnsArray = [referenceInvestmentObject];
+    for (
+      let timeReference = 1;
+      timeReference <= finalTimeHorizon;
+      timeReference++
+    ) {
+      const totalAmount =
+        returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+        monthlyContribution;
+      const interestReturns =
+        returnsArray[timeReference - 1].totalAmount * (finalReturnRate - 1);
+      const investedAmount = startingAmount + monthlyContribution * timeReference;
+      const totalInterestReturns = totalAmount - investedAmount;
+      returnsArray.push({
+        investedAmount,
+        interestReturns,
+        totalInterestReturns,
+        month: timeReference,
+        totalAmount,
+      });
     }
-    return returnsArrays;
-}
+  
+    return returnsArray;
+  }
